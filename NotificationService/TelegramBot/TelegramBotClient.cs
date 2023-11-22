a772f2c330cffc9eaed0120ad1710b23a51fb77d
YoshkiMatryoshki <yoshkimatryoshki@outlook.com>
2023-11-22 12:32:31 +0100

    feat: simple sending via telegram

--- NotificationService/TelegramBot/TelegramBotClient.cs
+++ NotificationService/TelegramBot/TelegramBotClient.cs
@@ -1,18 +1,43 @@
 using NotificationService.Interfaces.Bot;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace NotificationService.TelegramBot;
 
-internal class TelegramBotClient(IConfiguration configuration) : IBotClient
+internal class TelegramBotClient : IBotClient
 {
-    private readonly string botToken = configuration.GetConnectionString(Constants.BotTokenConnectionString) ?? string.Empty;
+    private readonly Telegram.Bot.TelegramBotClient botClient;
+
+    //TODO do not send if telegram init failed
+    public TelegramBotClient(IConfiguration configuration)
+    {
+        var botToken = configuration.GetConnectionString(Constants.BotTokenConnectionString) ?? string.Empty;
+        botClient = new Telegram.Bot.TelegramBotClient(botToken);
+    }
 
     public async Task<bool> SendNotifications(IEnumerable<long> userIds, string message, CancellationToken cancellation)
     {
+        if (!userIds.Any())
+            return false;
+
         foreach (var userId in userIds)
         {
-            Console.WriteLine($"NOTIFICATION SENT {userId}- {message}");
+            var chat = await GetTelegramChatByUserId(userId);
+
+            if (chat is null)
+            {
+                Console.WriteLine("Could not find chat by userID");
+                continue;
+            }
+
+            await botClient.SendTextMessageAsync(chat.Id, message, cancellationToken: cancellation);
         }
+
+        Console.WriteLine($"NOTIFICATIONs SENT - {message}");
 
         return true;
     }
+
+    private async Task<Chat?> GetTelegramChatByUserId(long telegramUserId)
+        => await botClient.GetChatAsync(telegramUserId);
 }
